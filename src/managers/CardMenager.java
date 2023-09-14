7665d7bbd4ae53ec2f9e3bed821b8c44bd56dd35
TheBialy3 <107372713+TheBialy3@users.noreply.github.com>
2023-09-14 10:03:26 +0200

    card class

---
+++ src/managers/CardMenager.java
@@ -0,0 +1,12 @@
+package managers;
+
+import scenes.Playing;
+
+public class CardMenager {
+    private Playing playing;
+    public CardMenager(Playing playing) {
+        this.playing = playing;
+
+
+    }
+}
