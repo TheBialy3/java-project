6ab8125eddda62d573a1071993acb792a67c8aa3
TheBialy3 <107372713+TheBialy3@users.noreply.github.com>
2023-09-27 15:15:22 +0200

    some upgrade fun

---
+++ src/xp/UpgradesItemsCard.java
@@ -0,0 +1,18 @@
+package xp;
+
+import objects.Card;
+
+import java.util.ArrayList;
+
+public class UpgradesItemsCard {
+    private ArrayList<Card> cards=new ArrayList<>();
+    public UpgradesItemsCard() {
+
+    }
+    public void createUpgraces(){
+
+    }
+
+
+
+}
