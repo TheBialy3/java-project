4220e866a538d1e781a2a2c340d44349e7218f1d
TheBialy3 <107372713+TheBialy3@users.noreply.github.com>
2023-10-24 20:52:23 +0200

    new enemy

---
+++ src/enemies/Bird.java
@@ -0,0 +1,13 @@
+package enemies;
+
+import managers.EnemyManager;
+import managers.WaveManager;
+
+import static helpz.Constants.EnemyType.*;
+
+public class Bird extends Enemy{
+    public Bird(float x, float y, int ID, EnemyManager em, WaveManager wm) {
+        super(x, y, ID, BIRD, em, wm);
+
+    }
+}
