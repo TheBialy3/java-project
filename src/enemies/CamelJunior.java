29b9a55a9e3c8ef7bc6d5c9f991cd012235992c0
TheBialy3karbi <karbialasol@gmail.com>
2023-12-14 19:51:31 +0100

    working on bestiary  trying to add bestiary layout in   button return in bestiary,   bestiary Rectangle bounds added   bestiary button in  menu added   new enemy

--- src/enemies/CamelJunior.java
+++ src/enemies/CamelJunior.java
@@ -5,9 +5,11 @@ import managers.WaveManager;
 
 import static helpz.Constants.EnemyType.*;
 
-public class CamelJuniorvv
+public class CamelJunior extends Enemy{
 
-
+    public CamelJunior(float x, float y, int ID, EnemyManager enemyManager, WaveManager waveManager) {
+        super(x, y, ID, CAMEL_JUNIOR, enemyManager, waveManager);
+    }
 
 
 }
