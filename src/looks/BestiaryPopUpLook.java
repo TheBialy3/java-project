b3b2bd3b82f0b8790c1821127bc9be9de5cd2e13
TheBialy3karbi <karbialasol@gmail.com>
2024-01-05 22:17:42 +0100

    working on bestiary  working on nwe enemy flying one BestiaryPopUpLook class added and modified cleanup
     new locked card idea
    
     need add good-looking button for
     every thing in asprite

---
+++ src/looks/BestiaryPopUpLook.java
@@ -0,0 +1,54 @@
+package looks;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class BestiaryPopUpLook {
+    private BufferedImage icon, background;
+    private EnemyBestiaryLook enemy=null;
+    private TowerBestiaryLook tower=null;
+    private int x, y;
+
+    public BestiaryPopUpLook(EnemyBestiaryLook enemy, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.enemy=enemy;
+        getEnenyBackground();
+    }
+
+
+
+    public BestiaryPopUpLook(TowerBestiaryLook tower, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.tower=tower;
+        getTowerBackground();
+    }
+
+    private void getEnenyBackground() {
+    }
+
+    private void getTowerBackground() {
+    }
+
+    public void draw(Graphics g) {
+        int nextLineH = 50;
+        int centerH = 100;
+        int centerW = 100;
+        int logoX = 820;
+        g.drawImage(background, x, y, null);
+        g.setColor(new Color(28, 28, 28));
+        g.setFont(new Font("Serif", Font.BOLD, 30));
+        if(tower==null){
+            g.drawString(enemy.getName(), x + centerW, y + centerH);
+            g.drawString(enemy.getHitboxName(), x + centerW * 5, y + centerH);
+            g.drawString(enemy.getMoveTypeName(), x + centerW, y + centerH + nextLineH);
+        } else {
+            g.drawString(tower.getName(), x + centerW, y + centerH);
+            g.drawString(tower.getCoolDownName(), x + centerW * 5, y + centerH);
+            g.drawString(tower.getDamageTypeName(), x + centerW, y + centerH + nextLineH);
+        }
+        g.drawImage(icon, x + logoX, y + nextLineH, null);
+    }
+}
+
