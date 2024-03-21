05b7dc34c60ca593b599b2f1d7881dc6275f4b96
TheBialy3karbi <karbialasol@gmail.com>
2024-03-21 20:55:50 +0100

    adding new enemy and tower need to add cards for new tower MySlider
    fixed red errors
    new card idea
    new locked card idea
    
    constant WORK
    
    need add good-looking
    button for every thing in
    asprite

--- src/ally/Knight.java
+++ src/ally/Knight.java
@@ -4,4 +4,14 @@ public class Knight extends Ally {
     public Knight(int x, int y, int allyType, int lvl) {
         super(x, y, allyType, lvl);
     }
+
+    public void power1() {
+
+    }
+    public void power2() {
+
+    } public void power3() {
+
+    }
+
 }
