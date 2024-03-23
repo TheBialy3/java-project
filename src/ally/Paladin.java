11fed2429780589c5938807b917ad574e0336d51
TheBialy3karbi <karbialasol@gmail.com>
2024-03-23 23:27:47 +0100

    adding new enemy and tower need to add cards for new tower MySlider
    fixed red errors
    new card idea
    new locked card idea
    
    constant WORK
    
    need add good-looking
    button for every thing in
    asprite

---
+++ src/ally/Paladin.java
@@ -0,0 +1,16 @@
+package ally;
+
+public class Paladin extends Ally{
+    public Paladin(int x, int y, int allyType, int lvl) {
+        super(x, y, allyType, lvl);
+    }
+
+    public void power1() {
+
+    }
+    public void power2() {
+
+    } public void power3() {
+
+    }
+}
